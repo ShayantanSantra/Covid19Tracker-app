0c557b8f3f5fb004bc9e0e48b05d22445fda0c6e
Shayantan Santra <shayantan.santra@gmail.com>
2020-07-31 20:46:56 +0530

    first commit

---
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.covid19tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    //UI views
+    private TextView titleTv;
+    private ImageButton refreshBtn;
+    private BottomNavigationView navigationView;
+    //
+    private Fragment homeFragment,statsFragment;
+    private Fragment activeFragment;
+    private FragmentManager fragmentManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //inint UI views
+        titleTv=findViewById(R.id.titleTv);
+        refreshBtn=findViewById(R.id.refreshBtn);
+        navigationView=findViewById(R.id.navigationView);
+        initFragments();
+        //refresh button click
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeFragment.onResume();
+
+            }
+        });
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private void initFragments() {
+        homeFragment = new HomeFragment();
+        statsFragment=new StatsFragment();
+        fragmentManager=getSupportFragmentManager();
+        activeFragment=homeFragment;
+        fragmentManager.beginTransaction()
+                .add(R.id.frame,homeFragment, "homeFragment")
+                .commit();
+        fragmentManager.beginTransaction()
+                .add(R.id.frame,statsFragment, "statsFragment").hide(statsFragment).commit();
+
+    }
+    private void loadHomeFragment(){
+        titleTv.setText("Home");
+        fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit();
+        activeFragment=homeFragment;
+    }
+    private void loadStatsFragment(){
+        titleTv.setText("COVID19 STATS");
+        fragmentManager.beginTransaction().hide(activeFragment).show(statsFragment).commit();
+        activeFragment=statsFragment;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //bottom
+        switch(item.getItemId()){
+            case R.id.nav_home:
+           loadHomeFragment();
+                return true;
+            case R.id.nav_stats:
+                loadStatsFragment();
+                return true;
+
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/covid19tracker/StatsFragment.java
@@ -0,0 +1,38 @@
+package com.example.covid19tracker;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StatsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StatsFragment extends Fragment {
+    Context context;
+    public StatsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context=context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_stats, container, false);
+        return view;
+    }
+}
